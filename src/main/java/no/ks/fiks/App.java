1047d44b88741cf199048c8e46f0b0c5d219f11d
Hama Keli <hama.keli@ks.no>
2021-09-28 14:51:43 +0200

    Cleaned up imports

--- src/main/java/no/ks/fiks/App.java
+++ src/main/java/no/ks/fiks/App.java
@@ -1,12 +1,7 @@
 package no.ks.fiks;
 
-
-import no.ks.fiks.Service.InsertTableService;
-
-import java.io.IOException;
-
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
     }
 }
