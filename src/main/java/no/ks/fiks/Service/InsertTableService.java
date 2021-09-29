6a6ff670bb3bf9556c485d9300474ba6e280950d
Hama Keli <hama.keli@ks.no>
2021-09-29 14:49:56 +0200

    Added a method to get biggest decimal value.

--- src/main/java/no/ks/fiks/Service/InsertTableService.java
+++ src/main/java/no/ks/fiks/Service/InsertTableService.java
@@ -3,6 +3,8 @@ package no.ks.fiks.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import no.ks.fiks.JsonStat.JsonStat;
+import no.ks.fiks.JsonStat.JsonStatDimension;
+import no.ks.fiks.JsonStat.JsonStatUnit;
 import no.ks.fiks.deserializer.JsonStatDeserializer;
 
 import java.io.IOException;
@@ -14,6 +16,7 @@ public class InsertTableService {
     private final List<JsonStat> jsonStat = new ArrayList<>();
     private boolean shouldIterate = false;
     private final List<Map<String[], BigDecimal>> sortedJsonStat = new ArrayList<>();
+    private int biggestDecimal = 0;
 
     public List<Map<String[], BigDecimal>> structureJsonStatTable(List<String> jsonList) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
@@ -25,6 +28,8 @@ public class InsertTableService {
             JsonStat mapperValue = mapper.readValue(json, JsonStat.class);
             jsonStat.add(mapperValue);
         }
+
+        getHighestDecimal();
 
         for (JsonStat stat : jsonStat) {
             String[][] table = new String[stat.getValues().size()][(stat.getId().size() * 2)];
@@ -89,5 +94,21 @@ public class InsertTableService {
             return increaseCategorySize(dimPosition, categorySize - 1, dimSizeIterator, stat);
         }
         return dimSizeIterator.get(dimPosition);
+    }
+
+    public List<JsonStat> getJsonStat() {
+        return jsonStat;
+    }
+
+    public int getBiggestDecimal() {
+        return biggestDecimal;
+    }
+
+    private void getHighestDecimal() {
+        getJsonStat().stream()
+                .flatMap(stat -> stat.getDimensions().stream())
+                .flatMap(jsonStatDimension -> jsonStatDimension.getJsonStatUnit().stream())
+                .filter(jsonStatUnit -> !jsonStatUnit.getDimensionName().isEmpty())
+                .forEach(jsonStatUnit -> biggestDecimal = Math.max(jsonStatUnit.getDecimals(), biggestDecimal));
     }
 }
